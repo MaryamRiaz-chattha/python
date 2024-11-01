542de958d2336c921be6fadbdbf32ee926924f25
MaryamRiaz-chattha <maryamriaz800@gmail.com>
2024-11-01 23:32:37 +0500

    python practice

---
+++ class1.py
@@ -0,0 +1,51 @@
+print("Maryam")
+print("riaz")
+#variable
+NAME="ali"
+NAME="aqsa"
+print(NAME)
+#no space
+#meaning full name
+print("""someone said 
+      "Ali" is a "good boy
+      but
+      he often make mistakes""")
+# task 1 (don't use triple quotes)
+#Someone Said:
+   #          "Ali" is good boy
+   #      but
+#he often make mistake
+
+print("Someone Said:\n\t\"Ali\" is a good boy\n\tbut\n\the often makes mistakes")
+#task 2 use variable to store your name and other info and print it in a meaningfull way
+name = "Maryam"
+age = 25
+email="maryamriazgmail.com"
+print(f"Hello, my name is {name} , my age is {age} and my email is {email}")#f string
+#primtive (number,string) and non primtive(tuple,array etc)
+number =123 #number
+print(type(number))
+num=1+2j#complex 
+print(type(num))
+#typecasting number/boolean etc can be converted into string but string cannot be converting into number
+number1=3
+string=str(number1)#type casting
+#operations arithemtic operator +,-,/,%
+a=5
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a **b)
+print(a/b)#answer in float
+print(a//b)#real number
+#comparitive > ,< ,==
+print(a>b)
+print(a<b)
+print(a==b)
+print(a>=b)
+print(a<=b)
+#unary
+a+=5
+print(a)
+
